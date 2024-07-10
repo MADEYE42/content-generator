e90ed45c0b9862fb5338b3b480e10b741e90371a
Gouresh  Madye <117588160+MADEYE42@users.noreply.github.com>
2024-07-10 15:09:16 +0530

    Display templates done

---
+++ ai-content-generator/app/dashboard/_components/SearchSection.tsx
@@ -0,0 +1,20 @@
+
+import { Search } from 'lucide-react'
+import React from 'react'
+
+function SearchSection({onSearchInput}:any) {
+  return (
+    <div className='p-10 bg-gradient-to-b from-black via-slate-500 to-slate-300 flex flex-col justify-center items-center text-white'>
+        <h2 className='text-3xl '>Browse the <span className='font-bold'>Templates</span>  you need.</h2>
+        <p>Lets dive into creative minds. </p>
+        <div className=' w-full flex justify-center items-center'>
+            <div className='flex gap-2 items-center p-2 border rounded-md mt-2 bg-white text-primary w-[50%]'>
+                <Search/>
+                <input type="text" placeholder='Search' onChange={(event)=>onSearchInput(event.target.value)} className='bg-transparent w-full outline-none'/>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default SearchSection
--- ai-content-generator/app/dashboard/page.tsx
+++ ai-content-generator/app/dashboard/page.tsx
@@ -1,9 +1,14 @@
-import React from 'react'
+"use client";
+import React, { useState } from 'react'
+import SearchSection from './_components/SearchSection'
+import Template from './_components/Template'
 
 function Dashboard() {
+  const [userSearchInput,setUserSearchInput]=useState<string>()
   return (
     <div>
-      
+        <SearchSection onSearchInput={(value:string)=>setUserSearchInput(value)}/>
+        <Template userSearchInput={userSearchInput}/>
     </div>
   )
 }
